00de9bc9a995957d90dfaf3ac027d6d013003d1e
Arwalk <siragentprovocateur@gmail.com>
2021-05-01 22:33:32 +0200

    Preparing the terrain for encoding.

--- src/protobuf.zig
+++ src/protobuf.zig
@@ -8,30 +8,41 @@ const ArrayList = std.ArrayList;
 pub const ProtoBuf = ArrayList(u8);
 
 
-pub const WireType = enum(u3){
-    Varint = 0,
-    Fixed64 = 1,
-    LenDelimited = 2,
-    Fixed32 = 5
+pub const FieldType = enum{
+    Varint,
+    Fixed64,
+    Fixed32,
+    SubMessage,
+    Bytes,
+    List,
+
+    pub fn get_wirevalue(ftype : FieldType) u3 {
+        return switch (ftype) {
+            .Varint => 0,
+            .Fixed64 => 1,
+            .Fixed32 => 5,
+            .SubMessage, .Bytes, .List => 2
+        };
+    }
 };
 
 pub const FieldDescriptor = struct {
     tag: u32,
     name: []const u8,
-    wtype: WireType,
+    ftype: FieldType,
 };
 
-pub fn fd(tag: u32, name: []const u8, wtype: WireType) FieldDescriptor {
+pub fn fd(tag: u32, name: []const u8, ftype: FieldType) FieldDescriptor {
     return FieldDescriptor{
         .tag = tag,
         .name = name,
-        .wtype = wtype
+        .ftype = ftype
     };
 }
 
 // encoding
 
-fn encode_varint(pb : *ProtoBuf, value: anytype) !void {
+fn append_varint(pb : *ProtoBuf, value: anytype) !void {
     var size_in_bits : u32 = @bitSizeOf(@TypeOf(value)) - @clz(@TypeOf(value), value);
 
     if(size_in_bits == 0){
@@ -49,12 +60,12 @@ fn encode_varint(pb : *ProtoBuf, value: anytype) !void {
 }
 
 
-fn _append(pb : *ProtoBuf, field: FieldDescriptor, value: anytype, allocator: *std.mem.Allocator) !void {
-    try encode_varint(pb, ((field.tag << 3) | @enumToInt(field.wtype)));
-    switch(field.wtype)
+fn append(pb : *ProtoBuf, field: FieldDescriptor, value: anytype, allocator: *std.mem.Allocator) !void {
+    try append_varint(pb, ((field.tag << 3) | field.ftype.get_wirevalue()));
+    switch(field.ftype)
     {
         .Varint => {
-            try encode_varint(pb, value);
+            try append_varint(pb, value);
         },
         else => @panic("Not implemented")
     }
@@ -69,12 +80,12 @@ pub fn pb_encode(data : anytype, allocator: *std.mem.Allocator) ![]u8 {
     inline for(field_list) |field| {
         if (@typeInfo(@TypeOf(@field(data, field.name))) == .Optional){
             if(@field(data, field.name)) |value| {
-                try _append(&pb, field, value, allocator);
+                try append(&pb, field, value, allocator);
             }
         }
         else
         {
-            try _append(&pb, field, @field(data, field.name), allocator);
+            try append(&pb, field, @field(data, field.name), allocator);
         }
     }
 
@@ -90,7 +101,7 @@ pub fn pb_encode(data : anytype, allocator: *std.mem.Allocator) ![]u8 {
 test "get varint" {
     var pb = ProtoBuf.init(testing.allocator);
     defer pb.deinit();
-    try encode_varint(&pb, @intCast(u32, 300));
+    try append_varint(&pb, @intCast(u32, 300));
 
     testing.expectEqualSlices(u8, &[_]u8{0b10101100, 0b00000010}, pb.items);
 }
