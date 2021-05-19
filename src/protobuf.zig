96eed03f1e2e88a9c6d75393d8720d492aa6c5e4
Arwalk <siragentprovocateurarwalk@gmail.com>
2021-05-19 14:59:26 +0200

    added another comptime check.
    Signed-off-by: Arwalk <siragentprovocateur@gmail.com>

--- src/protobuf.zig
+++ src/protobuf.zig
@@ -250,7 +250,7 @@ fn get_struct_field(comptime T: type, comptime field_name: []const u8 ) std.buil
         if(std.mem.eql(u8, field.name, field_name)) {
             break field;
         }
-    } else @compileError("This shouldnt happen");
+    } else @compileLog("Could not find field ", field_name, " in struct", T);
 }
 
 pub fn pb_init(comptime T: type, allocator : *std.mem.Allocator) T {
