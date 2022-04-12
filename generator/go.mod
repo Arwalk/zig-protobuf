a63374a7e1093e0596bb0a5bfd3eae82b6b547c7
Arwalk <siragentprovocateurarwalk@gmail.com>
2022-04-12 13:35:44 +0200

    Base of the generator is here.

---
+++ generator/go.mod
@@ -0,0 +1,5 @@
+module protoc-gen-zig
+
+go 1.17
+
+require google.golang.org/protobuf	v1.26.0
---
+++ generator/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("protoc-gen-zig is called")
+	
+}
