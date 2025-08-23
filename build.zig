9cbe8675428bf347b838e333cb7c8c55df3f7566
mochalins <117967760+mochalins@users.noreply.github.com>
2025-08-24 02:00:46 +0900

    test: Add root namespace tests

--- build.zig
+++ build.zig
@@ -122,6 +122,14 @@ pub fn build(b: *std.Build) !void {
                 .optimize = optimize,
             }),
         }),
+        b.addTest(.{
+            .name = "Root Namespace",
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("tests/root_namespace.zig"),
+                .target = target,
+                .optimize = optimize,
+            }),
+        }),
     };
 
     const convertStep = RunProtocStep.create(b, b, target, .{
