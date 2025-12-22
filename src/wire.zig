3391a97342992169be9a6dfb9e64a8fe93e0043b
Antoine GAGNIERE <antoine.gagniere@datadoghq.com>
2025-12-22 10:10:41 +0100

    std.meta.intToEnum -> std.enums.fromInt

--- src/wire.zig
+++ src/wire.zig
@@ -421,7 +421,7 @@ pub fn decodeRepeated(
                 var consumed: usize = 0;
                 while (consumed < bytes) {
                     const raw, const c = try decodeScalar(.int32, reader);
-                    const decoded = std.meta.intToEnum(Result, raw) catch {
+                    const decoded = std.enums.fromInt(Result, raw) orelse {
                         @branchHint(.cold);
                         return error.InvalidInput;
                     };
@@ -437,7 +437,7 @@ pub fn decodeRepeated(
             // Unpacked repeated enum.
             else {
                 const raw, const consumed = try decodeScalar(.int32, reader);
-                const decoded = std.meta.intToEnum(Result, raw) catch {
+                const decoded = std.enums.fromInt(Result, raw) orelse {
                     @branchHint(.cold);
                     return error.InvalidInput;
                 };
@@ -635,14 +635,14 @@ pub fn decodeMessage(
                     consumed += c;
                     const decoded = b: {
                         if (comptime field_ti == .optional) {
-                            break :b std.meta.intToEnum(
+                            break :b std.enums.fromInt(
                                 field_ti.optional.child,
                                 raw,
                             );
                         } else {
-                            break :b std.meta.intToEnum(Field, raw);
+                            break :b std.enums.fromInt(Field, raw);
                         }
-                    } catch {
+                    } orelse {
                         @branchHint(.cold);
                         return error.InvalidInput;
                     };
@@ -886,10 +886,10 @@ pub fn decodeMessage(
                                 const raw, const c =
                                     try decodeScalar(.int32, reader);
                                 consumed += c;
-                                const decoded = std.meta.intToEnum(
+                                const decoded = std.enums.fromInt(
                                     oo_field.type,
                                     raw,
-                                ) catch {
+                                ) orelse {
                                     @branchHint(.cold);
                                     return error.InvalidInput;
                                 };
